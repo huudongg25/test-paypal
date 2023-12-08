6c5b7601d482d98d0c4b27269bf1ff9558f838f9
huudongg25 <thdong98225@gmail.com>
2023-12-08 16:05:53 +0700

    update

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { PayPalScriptProvider } from '@paypal/react-paypal-js';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
+    <PayPalScriptProvider options={{ clientId: "AbN_6ZGUNdupX9n6gNqFUW8pWwGJEbMi901uoC57OWUq6tQEmS59cCoKvfKtl2gTQmGU3iosoyho0N12" }}>
+
     <App />
+    </PayPalScriptProvider>
   </React.StrictMode>
 );
 
